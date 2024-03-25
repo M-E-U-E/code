b96bd4530bb146488c626dc6edfc283d05a53761
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-03-25 19:47:54 +0600

    Add files via upload

---
+++ C. Assembly via Minimums.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+void solve()
+{
+    ll n;
+    cin>>n;
+
+    ll m =(n*(n-1))/ 2;
+    vector<ll>v(m + 1);
+
+    for (int i = 1; i <= m; i++)
+    {
+        cin >> v[i];
+    }
+
+    sort(v.begin()+1,v.begin()+m+1);
+
+    ll i = n - 1;
+    ll x = 1;
+    vector<ll>v2;
+    while (i != 0)
+    {
+        v2.push_back(v[x]);
+        x+= i;
+        i--;
+    }
+
+    for(auto p:v2)
+    {
+        cout << p << " ";
+    }
+
+    int  ans=v[m]+1000000;
+    cout << min(ans,1000000000)<<endl;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
+
